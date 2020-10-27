2c72c3d633ad2fbbe51c3ee16c8d275b852b5b1d
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2020-10-27 22:40:12 +0100

    function checkSpam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,8 +1,5 @@
-/**
- * checkSpam
- * @param {string} str base
- * @returns {boolean}
- */
 function checkSpam(str) {
-  // ваш код...
+  let str1 = str.toLowerCase();
+
+  return str1.includes('1xbet') || str1.includes('xxx');
 }
