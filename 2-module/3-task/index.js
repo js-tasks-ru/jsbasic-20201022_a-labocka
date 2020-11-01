4be55fa754f3dcb5fa752ec3036af026a72981be
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2020-11-01 21:59:39 +0100

    calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,15 @@
 let calculator = {
   // ваш код
+  sum () {
+    return this.a + this.b;
+  },
+  mul () {
+    return this.a * this.b;
+  },
+  read () {
+    this.a = 3;
+    this.b = 5;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
