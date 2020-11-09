2e40c7cdd991a80014c74783951fd874a793300a
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2020-11-09 19:06:10 +0100

    описание того, что вы сделали

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -4,15 +4,9 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
-<<<<<<< HEAD
-function showSalary(users, age) {
-  // ваш код...
-  
-=======
 function showSalary(data, age) {
   return data
     .filter(item => item.age <= age)
     .map(item => `${item.name}, ${item.balance}`)
     .join('\n');
->>>>>>> 87bef49e8806eae9d43390e297b0d4564a60a8fe
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,6 +1,44 @@
+const FIRST_COLUMN = 1;
+const SECOND_COLUMN = 2;
+const THIRD_COLUMN = 3;
 /**
  * Метод устанавливает необходимые по условию аттрибуты таблице
  * @param {Element} table
  */
 function highlight(table) {
+    const actions = {
+        [3]: (root, td) => {
+          if (td.dataset.available === 'true') {
+            root.classList.toggle('available', true);
+          } else if (td.dataset.available === 'false') {
+            root.classList.toggle('unavailable', true);
+          } else if (!td.hasAttribute('data-available')) {
+            root.hidden = true;
+          }
+        },
+        [2]: (root, td) => {
+          if (td.textContent === 'm') {
+            root.classList.toggle('male', true);
+          } else if (td.textContent === 'f') {
+            root.classList.toggle('female', true);
+          }
+        },
+        [1]: (root, td) => {
+          const age = parseInt(td.textContent, 10);
+    
+          if (age < 18) {
+            root.style.textDecoration = 'line-through';
+          }
+        },
+      };
+    
+      for (const tr of table.rows) {
+        Array.from(tr.cells).forEach((td, index) => {
+          const fn = actions[index];
+    
+          if (typeof fn === 'function') {
+            fn(tr, td);
+          }
+        });
+      }
 }
